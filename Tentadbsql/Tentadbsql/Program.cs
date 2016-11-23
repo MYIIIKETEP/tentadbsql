38ae1766f2f0205cbdc10fadeb7e56b4248657c5
Denis Jakusjev <qwarick@hotmail.com>
2016-11-23 08:49:58 +0100

    456

---
+++ Tentadbsql/Tentadbsql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tentadbsql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
